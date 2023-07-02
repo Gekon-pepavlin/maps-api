dcb767d954518928a71d922b133cc7252a3fc9e6
Petr Pavlín <pe.pavlin@gmail.com>
2023-07-02 23:52:20 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import useMap from "./core/useMap"
 import MarkerLayer from './core/MarkerLayer';
 import Marker from "./core/Marker";
+import {Map} from "leaflet"
 
 export {useMap, Marker, MarkerLayer};
-
+export type{Map}
