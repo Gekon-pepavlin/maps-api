e2c699149a3a8f28f81b2f8d30cc1ef3d5ff629b
Petr Pavlín <pe.pavlin@gmail.com>
2023-07-03 09:43:05 +0200

    Remove green border

--- src/core/Map.tsx
+++ src/core/Map.tsx
@@ -13,7 +13,7 @@ export default function Map(ref: any) {
         <div style={{position: "relative", height: "100%"}}>
 
             {/*Div for map to render*/}
-            <div ref={ref} style={{border: "5px solid green", position: "absolute", left: 0, top: 0, right: 0, bottom:0, overflow: "hidden"}}></div>
+            <div ref={ref} style={{position: "absolute", left: 0, top: 0, right: 0, bottom:0, overflow: "hidden"}}></div>
 
             {/*Div for panels and other custom elements*/}
             <div style={{position: "absolute", left:0, top:0,right:0, bottom:0, pointerEvents: "none"}}>
