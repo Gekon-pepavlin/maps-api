c4289f6e26e859bc356616fd6c7b059c10c24830
Petr Pavlín <pe.pavlin@gmail.com>
2023-07-15 11:01:46 +0200

    Fix projection export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import useMap from "./core/useMap"
 import MarkerLayer from './core/MarkerLayer';
 import Marker from "./core/Marker";
 import { MapOptions } from './core/Marker';
-import { projection } from './core/useMap';
 
-export {useMap, Marker, MarkerLayer, MapOptions, projection};
+export {useMap, Marker, MarkerLayer, MapOptions};
